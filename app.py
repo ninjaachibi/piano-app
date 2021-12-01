5fac6c0ce96f4e430dbefe6076f28039d29396a3
Robert L <1998.robert@gmail.com>
2021-11-30 23:12:17 -0500

    Change the welcome message

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# serve from public 
+
 @app.route("/")
 def index():
     print(__name__)
-    return '<h1>"Hello World!"</h1><div>ligma</div>'
+    return '<h1>hello this is the new version!</h1><div>ligma</div>'
 
