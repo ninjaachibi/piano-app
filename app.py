7f3841c8c6d24fd438a3114cb892296d77337261
Robert L <1998.robert@gmail.com>
2021-11-30 23:37:49 -0500

    get piano frontend to work

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+# from flask_cors import CORS #comment this on deployment
 
 app = Flask(__name__)
 
 # serve from public 
+app = Flask(__name__, static_url_path='', static_folder='frontend/public')
 
-@app.route("/")
-def index():
-    print(__name__)
-    return '<h1>hello this is the new version!</h1><div>ligma</div>'
+# CORS(app) #comment this on deployment
 
+
+# @app.route("/")
+# def index():
+#     print(__name__)
+#     return '<h1>hello this is the new version!'
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
